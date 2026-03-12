39abe87d25d641e518a3990b101d6b5c8b199250
Mr.NoBody <thieuefvanwhieues@gmail.com>
2026-03-12 18:35:26 +0700

    refactor: Loại bỏ import thừa

--- hms-backend/src/main/java/com/product/hms/dto/request/LoginRequest.java
+++ hms-backend/src/main/java/com/product/hms/dto/request/LoginRequest.java
@@ -3,8 +3,6 @@ package com.product.hms.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
