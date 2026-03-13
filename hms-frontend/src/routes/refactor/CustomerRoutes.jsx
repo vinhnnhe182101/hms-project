e3ef5614e52086b05a6287ee7e1276a1b7d12c17
Mr.NoBody <thieuefvanwhieues@gmail.com>
2026-03-13 12:23:01 +0700

    refactor: Chuyển các nhóm trong index.jsx thành các module riêng biệt

---
+++ hms-frontend/src/routes/refactor/CustomerRoutes.jsx
@@ -0,0 +1,33 @@
+import {Route} from "react-router-dom";
+import {CustomerLayout} from "../../layouts/customer/CustomerLayout.jsx";
+import HomePage from "../../pages/customer/HomePage.jsx";
+import {ProtectedRoute} from "../ProtectedRoute.jsx";
+import RoomsPage from "../../pages/customer/RoomsPage.jsx";
+import BookingHistoryPage from "../../pages/customer/BookingHistoryPage.jsx";
+
+function BookingDetailPage() {
+    return null;
+}
+
+export const CustomerRoutes = () => {
+    return (
+            <>
+                <Route path="/" element={<CustomerLayout/>}>
+                    <Route index element={<HomePage/>}/>
+                    <Route path="rooms" element={<RoomsPage/>}/>
+                </Route>
+
+                <Route
+                        path="/customer"
+                        element={
+                            <ProtectedRoute>
+                                <CustomerLayout/>
+                            </ProtectedRoute>
+                        }
+                >
+                    <Route path="bookings" element={<BookingHistoryPage/>}/>
+                    <Route path="bookings/:id" element={<BookingDetailPage/>}/>
+                </Route>
+            </>
+    );
+};
