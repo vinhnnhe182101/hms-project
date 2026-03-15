78a4094adec0f41fcc2aad1d20946957656f66c1
Mr.NoBody <thieuefvanwhieues@gmail.com>
2026-03-15 11:54:10 +0700

    chore: đổi lại tên file bị sai models.d.ts

--- hms-frontend/src/dto/customs.d.ts
+++ hms-frontend/src/dto/customs.d.ts
@@ -1,4 +1,4 @@
-import { LucideIcon } from "lucide-react";
+import {LucideIcon} from "lucide-react";
 import React from "react";
 
 declare global {
@@ -56,4 +56,61 @@ declare global {
         icon: LucideIcon;
         element: React.JSX.Element;
     };
+
+    type ListContextType<PageType, SearchParamsType> = {
+        page: PageType;
+        setPage: React.Dispatch<React.SetStateAction<PageType>>;
+        searchParams: SearchParamsType;
+        setSearchParams: React.Dispatch<React.SetStateAction<SearchParamsType>>;
+    };
+
+    type ListFactoryType<PageType, SearchParamsType> = {
+        Provider: React.FC<{ children: React.ReactNode }>;
+        useList: () => ListContextType<PageType, SearchParamsType>;
+    };
+
+    /**
+     * PageableRequest: Định nghĩa kiểu dữ liệu cho yêu cầu phân trang khi gọi API.
+     *
+     * Các trường:
+     * - page: Số trang hiện tại (bắt đầu từ 0). Nếu không cung cấp, mặc định là 0.
+     * - size: Số lượng phần tử trên mỗi trang. Nếu không cung cấp, mặc định là 20.
+     * - sort: Chuỗi định dạng để chỉ định trường và thứ tự sắp xếp (ví dụ: "name,asc" hoặc "createdDate,desc").
+     */
+    type PageableRequest = {
+        page?: number;
+        size?: number;
+        sort?: string;
+    };
+
+    type PageResponse<ContentType> = {
+        content: ContentType[];
+        pageable: {
+            sort: {
+                empty: boolean;
+                sorted: boolean;
+                unsorted: boolean;
+            };
+            offset: number;
+            pageNumber: number;
+            pageSize: number;
+            paged: boolean;
+            unpaged: boolean;
+        };
+        last: boolean;
+        totalElements: number;
+        totalPages: number;
+        size: number;
+        number: number; // Page index hiện tại (bắt đầu từ 0)
+        sort: {
+            empty: boolean;
+            sorted: boolean;
+            unsorted: boolean;
+        };
+        first: boolean;
+        numberOfElements: number;
+        empty: boolean;
+    };
+
+    type ReservationSearchParams = ReservationSearchFilter & PageableRequest;
 }
